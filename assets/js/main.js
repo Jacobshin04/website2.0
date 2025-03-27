cf3f8a27f0d5ea7e41228ab1216f0b215d2f1b8e
Jacobshin04 <139908998+Jacobshin04@users.noreply.github.com>
2025-03-27 14:37:07 -0700

    done putting content

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,28 @@
 */
 !(function($) {
   "use strict";
+  // Porfolio isotope and filter
+  $(document).ready(function () {
+    var portfolioIsotope = $('.portfolio-container').isotope({
+      itemSelector: '.portfolio-item',
+      layoutMode: 'fitRows'
+    });
+  
+    $('#portfolio-flters li').on('click', function () {
+      $('#portfolio-flters li').removeClass('filter-active');
+      $(this).addClass('filter-active');
+  
+      portfolioIsotope.isotope({
+        filter: $(this).data('filter')
+      });
+    });
+  });
+  
+  // Initiate venobox (lightbox feature used in portofilo)
+  $(document).ready(function() {
+    $('.venobox').venobox();
+  });
+
 
   // Nav Menu
   // $(document).on('click', '.nav-menu a, .mobile-nav a', function(e) {
@@ -124,28 +146,4 @@
       }
     }
   });
-
-  // Porfolio isotope and filter
-  $(window).on('load', function() {
-    var portfolioIsotope = $('.portfolio-container').isotope({
-      itemSelector: '.portfolio-item',
-      layoutMode: 'fitRows'
-    });
-
-    $('#portfolio-flters li').on('click', function() {
-      $("#portfolio-flters li").removeClass('filter-active');
-      $(this).addClass('filter-active');
-
-      portfolioIsotope.isotope({
-        filter: $(this).data('filter')
-      });
-    });
-
-  });
-
-  // Initiate venobox (lightbox feature used in portofilo)
-  $(document).ready(function() {
-    $('.venobox').venobox();
-  });
-
 })(jQuery);
